dd127a56d9886411d05e214a643480338451a2e5
Nikhil Sonti <nikhilsv92@gmail.com>
2025-10-10 10:47:25 -0700

    browseros settings page url display update

--- packages/browseros/chromium_patches/chrome/browser/extensions/browseros_extension_constants.h
+++ packages/browseros/chromium_patches/chrome/browser/extensions/browseros_extension_constants.h
@@ -1,15 +1,19 @@
 diff --git a/chrome/browser/extensions/browseros_extension_constants.h b/chrome/browser/extensions/browseros_extension_constants.h
 new file mode 100644
-index 0000000000000..79dc812fae32f
+index 0000000000000..6505926e1890f
 --- /dev/null
 +++ b/chrome/browser/extensions/browseros_extension_constants.h
-@@ -0,0 +1,48 @@
+@@ -0,0 +1,65 @@
 +// Copyright 2024 The Chromium Authors
 +// Use of this source code is governed by a BSD-style license that can be
 +// found in the LICENSE file.
 +
 +#ifndef CHROME_BROWSER_EXTENSIONS_BROWSEROS_EXTENSION_CONSTANTS_H_
 +#define CHROME_BROWSER_EXTENSIONS_BROWSEROS_EXTENSION_CONSTANTS_H_
++
++#include <optional>
++#include <string>
++#include <vector>
 +
 +namespace extensions {
 +namespace browseros {
@@ -46,6 +50,19 @@ index 0000000000000..79dc812fae32f
 +    kAISidePanelExtensionId,
 +    kBugReporterExtensionId
 +  };
++}
++
++// Get display name for BrowserOS extensions in omnibox
++// Returns the display name if extension_id is a BrowserOS extension,
++// otherwise returns std::nullopt
++inline std::optional<std::string> GetExtensionDisplayName(
++    const std::string& extension_id) {
++  if (extension_id == kAISidePanelExtensionId) {
++    return "BrowserOS/settings";
++  } else if (extension_id == kBugReporterExtensionId) {
++    return "BrowserOS/bug-reporter";
++  }
++  return std::nullopt;
 +}
 +
 +}  // namespace browseros
